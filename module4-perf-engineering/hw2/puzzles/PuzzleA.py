577965ec0c70a7ac6d83304e84569a6fd9b94ab8
Ayman Patel <ayman.patel97@outlook.com>
2026-06-22 22:31:43 +0100

    feat: fix puzzle a

--- module4-perf-engineering/hw2/puzzles/PuzzleA.py
+++ module4-perf-engineering/hw2/puzzles/PuzzleA.py
@@ -1,57 +1,28 @@
+%%writefile ring_allreduce.py
+
+import os
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.distributed as dist
+from torch.distributed.device_mesh import init_device_mesh
 
 
-class ColumnParallelLinear(nn.Module):
-    def __init__(
-            self,
-            input_size: int,
-            output_size: int,
-            tp_mesh: dist.DeviceMesh,
-        ) -> None:
-        super().__init__()
-        # Your code is here
-        assert self.weight.data.shape[1] == input_size
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # Your code is here
-        pass
-
-class RowParallelLinear(nn.Module):
-    def __init__(
-            self,
-            input_size: int,
-            output_size: int,
-            tp_mesh: dist.DeviceMesh,
-        ) -> None:
-        super().__init__()
-        # Your code is here
-        raise NotImplementedError
-        assert isinstance(self.weight, torch.Tensor)
+def ring_allreduce(x: torch.Tensor, pg: dist.ProcessGroup):
+    # Your code is here
+    pass
 
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # Your code is here
-        raise NotImplementedError
+def main():
+    world_size = int(os.environ["WORLD_SIZE"])
+    mesh = init_device_mesh("cpu", mesh_shape=(world_size,), mesh_dim_names=("dp",))
 
+    rank = dist.get_rank()
+    x = (torch.arange(4) + rank * 4).float()
+    print(f"[rank {rank}] allreduce input: {x}", flush=True)
 
-class ParallelTransformerMLP(nn.Module):
-    def __init__(
-        self,
-        hidden_size: int,
-        intermediate_size: int,
-        tp_mesh: dist.DeviceMesh,
-        dropout: float = 0.1,
-    ) -> None:
-        super().__init__()
-        self.fc1 = None # Your code is here
-        self.act = nn.GELU()
-        self.fc2 = None # Your code is here
-        self.dropout = nn.Dropout(dropout)
-        raise NotImplementedError
+    pg = mesh.get_group("dp")
+    ring_allreduce(x, pg)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # Your code is here
-        raise NotImplementedError
+    print(f"[rank {rank}] allreduce output: {x}", flush=True)
+
+if __name__ == "__main__":
+    main()
